c29e8cc5ee8c9482c27d6bb1e843d6813023606b
Razorback360 <razorback@movies4discord.tk>
2026-05-03 18:44:57 +0300

    add github actions

--- gitInitMono.bat
+++ gitInitMono.bat
@@ -5,4 +5,4 @@ git init
 git add .
 git commit -m "Monorepo split commit"
 git remote add origin https://github.com/SWE455-proj-team/%CURRENT_DIR%.git
-git push -u origin master
+git push -u origin main
